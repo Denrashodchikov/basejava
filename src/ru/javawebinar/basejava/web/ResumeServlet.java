beb7bed25470ce87937674d7f281e851bbf2a6ee
den1so <denrashodchikov@ya.ru>
2024-05-15 15:08:54 +0300

    #denrashodchikov hw_15_3 -ResumeServlet: заведи переменую и иницаилизируй ее в методе init(ServletConfig config) от HttpServlet

--- src/ru/javawebinar/basejava/web/ResumeServlet.java
+++ src/ru/javawebinar/basejava/web/ResumeServlet.java
@@ -4,6 +4,7 @@ import ru.javawebinar.basejava.Config;
 import ru.javawebinar.basejava.model.Resume;
 import ru.javawebinar.basejava.storage.Storage;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,6 +14,14 @@ import java.io.PrintWriter;
 import java.util.List;
 
 public class ResumeServlet extends HttpServlet {
+
+    private Storage storage;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        storage = Config.get().getStorage();
+    }
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException {
         request.setCharacterEncoding("UTF-8");
@@ -21,7 +30,6 @@ public class ResumeServlet extends HttpServlet {
         //String name = request.getParameter("name");
         //response.getWriter().write(name == null ? "Hello resumes!" : "Hello " + name + "!");
 
-        Storage storage = Config.get().getStorage();
         List<Resume> listResumes = storage.getAllSorted();
         PrintWriter out = response.getWriter();
         out.print("""
